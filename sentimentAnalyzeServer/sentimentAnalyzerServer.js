c193e62181221cc2ed0bfc3e55d04313c80bd586
sarang barshikar <skbarshikar@mitaoe.ac.in>
2021-07-20 05:50:11 +0000

    task1:define & implement getNLUInstance

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,26 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv')
 
+const app = new express();
+function getNLUInstance(){
+    
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+    dotenv.config();
+
+    let api_key = process.env.API_KEY
+    let api_url = process.env.API_URL 
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version:'2020-08-01',
+        authenticator:new IamAuthenticator({
+            apikey:api_key,
+        }),
+        serviceUrl:api_url,
+    });
+    return naturalLanguageUnderstanding;
+
+}
 app.use(express.static('client'))
 
 const cors_app = require('cors');
